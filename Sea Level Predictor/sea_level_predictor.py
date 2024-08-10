9b61a018ac4bde77913e801927559d1f4236650a
Oizys13 <brchreda@gmail.com>
2024-08-10 08:50:05 +0100

    altered sea level predictor

--- Sea Level Predictor/sea_level_predictor.py
+++ Sea Level Predictor/sea_level_predictor.py
@@ -5,32 +5,32 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 import numpy as np
 
-def draw_plot():
-    # Read data from file
-    df = pd.read_csv('epa-sea-level.csv')
+def generate_sea_level_plot():
+    # Load data
+    data = pd.read_csv('epa-sea-level.csv')
 
-    # Create scatter plot
-    fig = plt.figure()
-    plt.scatter(df['Year'],df['CSIRO Adjusted Sea Level'],s=8)
+    # Initialize plot
+    figure = plt.figure()
+    plt.scatter(data['Year'], data['CSIRO Adjusted Sea Level'], s=8)
 
-    # Create first line of best fit
-    res1 = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    x1 = np.arange(df['Year'].min(),2051,1)
-    y1 = res1.intercept + res1.slope*x1
-    plt.plot(x1,y1,color='firebrick')
+    # Compute and plot the first line of best fit
+    first_fit = linregress(data['Year'], data['CSIRO Adjusted Sea Level'])
+    x_values1 = np.arange(data['Year'].min(), 2051, 1)
+    y_values1 = first_fit.intercept + first_fit.slope * x_values1
+    plt.plot(x_values1, y_values1, color='firebrick')
 
-    # Create second line of best fit
-    df2 = df[df['Year']>=2000]
-    res2 = linregress(df2['Year'], df2['CSIRO Adjusted Sea Level'])
-    x2 = np.arange(df2['Year'].min(),2051,1)
-    y2 = res2.intercept + res2.slope*x2
-    plt.plot(x2,y2,color='mediumseagreen')
+    # Compute and plot the second line of best fit
+    recent_data = data[data['Year'] >= 2000]
+    second_fit = linregress(recent_data['Year'], recent_data['CSIRO Adjusted Sea Level'])
+    x_values2 = np.arange(recent_data['Year'].min(), 2051, 1)
+    y_values2 = second_fit.intercept + second_fit.slope * x_values2
+    plt.plot(x_values2, y_values2, color='mediumseagreen')
 
-    # Add labels and title
+    # Set labels and title
     plt.xlabel('Year')
     plt.ylabel('Sea Level (inches)')
-    plt.title('Rise in Sea Level')
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
+    plt.title('Sea Level Increase')
+
+    # Save plot and provide access to the axis object for testing
+    plt.savefig('sea_level_increase.png')
     return plt.gca()
